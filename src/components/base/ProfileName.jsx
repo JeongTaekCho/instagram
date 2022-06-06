f66c13bbdded5ab6fb856922e574ee3eb1a9fecc
JeongTaekCho <92679073+JeongTaekCho@users.noreply.github.com>
2022-06-06 16:59:31 +0900

    add feed

---
+++ src/components/base/ProfileName.jsx
@@ -0,0 +1,15 @@
+const ProfileName = () => {
+      return(
+            <>
+                  <div className="profileName">
+                        <div className="profileImg"></div>
+                        <div className="profileInfo">
+                              <p className="pName">프로필 아이디</p>
+                              <p className="pInfo"></p>
+                        </div>
+                  </div>
+            </>
+      )
+}
+
+export default ProfileName;
